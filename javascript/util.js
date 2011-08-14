6f87b071f6836230d60a693216adeec19d5f7639
liunian <dengjij@gmail.com>
2011-08-14 16:34:50 +0800

    add util.js

---
+++ javascript/util.js
@@ -0,0 +1,15 @@
+/**
+ *    Here are some util functions
+ *    @author dengjij@gmail.com
+ */
+
+
+/**
+ *  A simple function to merge old object with new object
+ */
+function mergeObj(oldObj, newObj) {
+    for (var item in newObj) {
+        oldObj[item] = newObject[item];
+    }
+    return this;
+}
