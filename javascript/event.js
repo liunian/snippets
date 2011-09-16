b4328006578f1bf0a9432c2c27f450e7715816d9
liunian <dengjij@gmail.com>
2011-09-16 22:36:10 +0800

    add {} for if-else in event.js

--- javascript/event.js
+++ javascript/event.js
@@ -2,25 +2,27 @@
  *    Some event functions from others or mine
  *    @author dengjij@gmail.com
  */
- 
 
 /**
  *  Simple add / remove event from PPK
  */
-function addEventSimple(obj,evt,fn) {
-	if (obj.addEventListener)
-		obj.addEventListener(evt,fn,false);
-	else if (obj.attachEvent)
-		obj.attachEvent('on'+evt,fn);
+function addEventSimple(obj, evt, fn) {
+	if (obj.addEventListener) {
+		obj.addEventListener(evt, fn, false);
+	}
+	else if (obj.attachEvent) {
+		obj.attachEvent('on' + evt, fn);
+	}
 }
 
-function removeEventSimple(obj,evt,fn) {
-	if (obj.removeEventListener)
-		obj.removeEventListener(evt,fn,false);
-	else if (obj.detachEvent)
-		obj.detachEvent('on'+evt,fn);
+function removeEventSimple(obj, evt, fn) {
+	if (obj.removeEventListener) {
+		obj.removeEventListener(evt, fn, false);
+	}
+	else if (obj.detachEvent) {
+		obj.detachEvent('on' + evt, fn);
+	}
 }
-
 
 /**
  *  Stop event bubble, if ie, use e.cancelBubble = true; while other's following
