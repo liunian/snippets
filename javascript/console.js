fd6e50d57991a7dfa138d7b681459504a0925c49
liunian <dengjij@gmail.com>
2011-09-28 23:40:24 +0800

    a big change of console.js

--- javascript/console.js
+++ javascript/console.js
@@ -1,29 +1,105 @@
-// This is just a "poor man's console" so we can test in IE6.
-var console = (function (cn, undef) {
-    
-    function isBool(obj) {
-        return obj === !!obj;
-    }
+/**
+ * a simple console for ie
+ *
+ * requires util.js
+ */
 
-    if (cn.log === undef) {
-        cn.log = function () {
-            var renderedArgs = [],
-                i = 0,
-                il = arguments.length;
-            
-            for (; i < il; i += 1) {
-                if (isBool(arguments[i])) {
-                    renderedArgs.push(arguments[i] ? 'true' : 'false');
-                } else {
-                    renderedArgs.push(arguments[i]);
-                }
-            }
-            
-            alert(renderedArgs.join('\n'));
-            
-        };
-    }
-    
-    return cn;
-    
-}(typeof console === "undefined" ? {} : console));
+var console = function(console) {
+	if (typeof console !== 'undefined') {
+		return console;
+	}
+
+	var panel; 
+
+	/**
+	 * create the console panel as first run
+	 */
+	var _init = function() {
+		var panel = document.createElement('div');
+		panel.className = 'consoleLog';
+		panel.style.cssText = '; position: absolute; top: 0; right: 0; background:rgb(223,223,223); color:#333; width: 600px; height: 200px; overflow:auto';
+		document.body.appendChild(panel);
+		return panel;
+	};
+
+	panel = _init();
+
+	/**
+	 * append each log to the panel content
+	 */
+	var appendLog = function(msg) {
+		var perLog = document.createElement('div');
+		perLog.innerHTML = msg;
+		panel.appendChild(perLog);
+	};
+
+	var argType = function(arg){
+		return isBool(arg) ? 'bool' : isArray(arg) ? 'array' : isElement(arg) ? 'node' : isObject(arg) ? 'object' : 'other';
+	};
+
+	/**
+	 * render an obj
+	 */
+	var render = function(obj){
+		var result = '',
+			objType = argType(obj);
+
+		switch(objType){
+			case "bool": 
+				result += renderBoolean(obj);
+				break;
+			case 'array':
+				result += renderArray(obj);
+				break;
+			case 'node':
+				result += renderNode(obj);
+				break;
+			case 'object': 
+				result += renderObject(obj);
+				break;
+			case 'other':
+			default:
+				result += renderDefault(obj);
+				break;
+		}
+		return result;
+	};
+
+	var renderArray = function(arr){
+
+	};
+
+	var renderNode = function(node){
+		return outerHTML(node).replace(/</g, '&lt;');
+	};
+
+	var renderObject = function(obj){
+
+	};
+
+	var renderBoolean = function(bool){
+		return bool ? 'true' : 'false';
+	};
+
+	var renderDefault = function(obj){
+		return obj;
+	}
+
+	var log = function() {
+		var renderedArgs = [],
+			i = 0,
+			result = '';
+			il = arguments.length;
+
+		for (; i < il; i += 1) {
+			renderedArgs.push(render(arguments[i]));
+		}
+
+		result = renderedArgs.join('\n');
+		appendLog(result);
+	};
+
+	return {
+		log: log
+	}
+} (console);
