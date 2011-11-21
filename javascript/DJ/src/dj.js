9ac05c1997303b40e221f310207a33cd2ff7f8a5
liunian <dengjij@gmail.com>
2011-11-22 00:30:39 +0800

    fix export method

--- javascript/DJ/src/dj.js
+++ javascript/DJ/src/dj.js
@@ -11,6 +11,9 @@ var DJ = {
 
 /**
  *  export modules
+ *
+ *  can take two parameters: moduleName, module content;
+ *  or take a key-value object to export more than one per time
  */
 DJ.export = function() {
     var args = arguments;
@@ -18,7 +21,7 @@ DJ.export = function() {
     if (args.length == 2) {
         DJ[args[0]] = args[1];
     } else if (args.length == 1 &&
-        Object.prototype.toString.call(args[0]) == '[Object Object]') {
+        Object.prototype.toString.call(args[0]) == '[object Object]') {
         var obj = args[0];
         for (var name in obj) {
             DJ[name] = obj[name];
