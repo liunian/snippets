ffaf5e30c3533e85fde73562d3e94d88510af449
liunian <dengjij@gmail.com>
2012-08-24 19:04:22 +0800

    add test for dj.js with mocha and chai

---
+++ javascript/DJ/test/module/test.dj.js
@@ -0,0 +1,29 @@
+describe('DJ', function() {
+    describe('DJ.add', function() {
+        it('add one module', function() {
+            DJ.add('add_t1', function() { return 1;});
+            expect(DJ.add_t1()).to.be.eql(1);
+        });
+
+        it('add more than one modules', function() {
+            DJ.add({
+                add_t2: function() {
+                    return 2;
+                },
+                add_t3: function() {
+                    return 3;
+                }
+            });
+            expect(DJ.add_t2()).to.be.eql(2);
+            expect(DJ.add_t3()).to.be.eql(3);
+        });
+        it('add modules with parameters', function() {
+            DJ.add({
+                add_t4: function(a, b) {
+                    return a + b;
+                }
+            });
+            expect(DJ.add_t4(10, 20)).to.be.eql(30);
+        });
+    });
+});
