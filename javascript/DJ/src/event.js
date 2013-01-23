f76eea46aed79d4e6188865390971356a02bff12
liunian <dengjij@gmail.com>
2013-01-23 17:19:12 +0800

    save event map and add fire feature
    add _uuid property for function, which makes possiable to remove wrapFn
    attach by attachEvent in ie.
    
    add fire method, which means can trigger event by using code, and then
    the code can be tested

--- javascript/DJ/src/event.js
+++ javascript/DJ/src/event.js
@@ -3,6 +3,11 @@
  *
  */
 (function($) {
+    // event fn id
+    var _uuid = 1;
+    // event maps, key is _uuid, value is fn
+    var eventsMap = {};
+
     var event = {
         on: null,
         off: null,
@@ -37,11 +42,14 @@
     // do browser feature detected only once
     if (typeof window.addEventListener === 'function') {
         event.on = function(obj, type, fn) {
+            fn._uuid = _uuid;
+            eventsMap[_uuid++] = fn;
             obj.addEventListener(type, fn, false);
         };
 
         event.off = function(obj, type, fn) {
             obj.removeEventListener(type, fn, false);
+            delete eventsMap[fn._uuid];
         };
 
         event.stopBubble = function(e) {
@@ -56,6 +64,8 @@
             var wrapFn = function() {
                 fn.call(obj, window.event);
             };
+            fn._uuid = _uuid;
+            eventsMap[_uuid++] = wrapFn;
             obj.attachEvent('on' + type, wrapFn);
         };
 
@@ -71,6 +81,28 @@
             window.event.returnValue = false;
         };
     }
+
+    /**
+     * @method fire 触发事件，这里仅是 mouseEvent
+     * @param {Element} ele 触发事件的 dom 元素
+     * @param {Strintg} type 事件类型，如 `mouseover` 和 `click` 等
+     */
+    event.fire = function fireEvent(ele, type) {
+        var doc = document,
+            e;
+
+        // ie9 及其后支持 DOM3 的 createEvent 和 dispatchEvent 了
+        // http://msdn.microsoft.com/zh-cn/library/ff975304.aspx
+        // http://msdn.microsoft.com/zh-cn/library/ff975459.aspx
+        // https://developer.mozilla.org/en-US/docs/DOM/document.createEvent
+        if(typeof doc.createEvent === 'function') {
+            e = doc.createEvent('MouseEvents');
+            e.initMouseEvent(type, true, true, document.defaultView, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+            ele.dispatchEvent(e);
+        } else {
+            ele.fireEvent('on' + type, doc.createEventObject());
+        }
+    };
 
     /**
      * only support #id, tagName, .className
